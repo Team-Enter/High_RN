d7ff9269a6abee86d1e7c1d62a8ebce3083b5046
yang-yunho <yangyoonho0710@naver.com>
2024-02-04 01:51:13 +0900

    📝 :: InputTextField component 수정

--- HIGH/src/components/InputTextField.js
+++ HIGH/src/components/InputTextField.js
@@ -1,15 +1,19 @@
-import { View, TextInput, StyleSheet } from 'react-native'
+import { View, TextInput, StyleSheet, Text } from 'react-native'
 import React from 'react'
 
 // 입력 텍스트 필드
-const InputTextField = ({placeholder}) => {
+const InputTextField = ({placeholder, label, errorMessage}) => {
   return (
     <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
       <TextInput
         style={styles.input}
         placeholder={placeholder}
         placeholderTextColor="#D0D0D0"
       />
+      {errorMessage ? (
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
+      ) : null}
     </View>
   )
 }
@@ -27,15 +31,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 10,
   },
+  label:{
+    fontFamily: 'Roboto-Medium',
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#000',
+    textAlign: 'left',
+  },
   input: {
     flex: 1,
     fontSize: 16,
     color: '#000',
     textAlign: 'left',
-    fontFamily: 'Inter-Regular',
+    fontFamily: 'Roboto-Medium',
     fontWeight: '500',
     lineHeight: 22,
     letterSpacing: -0.408,
+  },
+  errorMessage:{
+    fontFamily: 'Roboto-Medium',
+    fontSize: 13,
+    lineHeight: 24,
+    color: '#F04621',
+    textAlign: 'left',
   },
 })
 
