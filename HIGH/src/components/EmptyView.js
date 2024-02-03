c10f2103b88e95e979ef6e9d7d2254b3047dd589
yang-yunho <yangyoonho0710@naver.com>
2024-02-04 05:35:26 +0900

    📝 :: EmptyView component 추가

---
+++ HIGH/src/components/EmptyView.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EmptyView = ({height}) => {
+  return (
+    <View style={{height: height}}/>
+  )
+}
+
+export default EmptyView
