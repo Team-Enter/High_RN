c04940280a0962b3d9881c92c3fa7a1b6dc0b2b7
yang-yunho <yangyoonho0710@naver.com>
2024-02-04 05:57:02 +0900

    📝 :: BackButton component 추가

--- HIGH/src/components/BackButton.js
+++ HIGH/src/components/BackButton.js
@@ -1,13 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 
 // 전으로 돌아가기 버튼_비활성화 상태
-const BackButton = () => {
+const BackButton = ({onPress, disabled}) => {
   return (
-    <View>
-      <Text>BackButton</Text>
-    </View>
+    <TouchableOpacity
+    style={[styles.button, disabled && styles.disabledButton]}
+    onPress = {onPress}
+    disabled={disabled}
+  >
+    <Text style={styles.buttonText}>← 이전</Text>
+  </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 0,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 0,
+    width: 77,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: 10,
+    backgroundColor: '#0080F7'
+  },
+  disabledButton: {
+    backgroundColor: '#0362D2'
+  },
+  buttonText: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontFamily: 'Inter-Regular',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+  }
+})
 
 export default BackButton
