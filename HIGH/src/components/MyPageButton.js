ac2cb9c1d5cf83eec0fef29a624d72dd7b9c027e
yang-yunho <yangyoonho0710@naver.com>
2024-02-05 07:08:08 +0900

    📝 :: PeopleInfo component 추가

---
+++ HIGH/src/components/MyPageButton.js
@@ -0,0 +1,32 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+
+const MyPageButton = ({title, onPress}) => {
+  return (
+    <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+    >
+        <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+    button:{
+        width: 165,
+        height: 36,
+        backgroundColor: '#E8F4FF',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text:{
+        fontFamily: 'Inter-Regular',
+        fontWeight: 'bold',
+        fontSize: 15,
+        color: '#0081F9'
+    },
+})
+
+export default MyPageButton
