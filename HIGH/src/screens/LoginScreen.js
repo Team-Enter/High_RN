6fcfea99b810bff0327580c2146e206989f4b346
yang-yunho <yangyoonho0710@naver.com>
2024-02-09 01:05:44 +0900

    ⚡️ :: 로그인 API 연동

--- HIGH/src/screens/LoginScreen.js
+++ HIGH/src/screens/LoginScreen.js
@@ -1,7 +1,7 @@
-import { View, StyleSheet, Image } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, Image, Alert } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import LogoText from '../components/LogoText'
+// import LogoText from '../components/LogoText'
 import Title from '../components/Title'
 import InputTextField from '../components/InputTextField'
 import DefaultButton from '../components/DefaultButton'
@@ -10,9 +10,58 @@ import { useNavigation } from '@react-navigation/native'
 // 로그인
 const LoginScreen = () => {
 
+  const [accountId, setAccountId] = useState('');
+  const [password, setPassword] = useState('');
+
   const navigation = useNavigation();
-  const handleMainPage = () => {
-    navigation.navigate('MainPageScreen');
+
+  const handleMainPage = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify({
+          accountId: accountId,
+          password: password,
+        }),
+      });
+
+      if(response.ok){
+        const responseData = await response.json();
+        // 로그인 성공
+        if (response.status === 201){
+          Alert.alert('로그인 성공', '로그인에 성공하였습니다.');
+
+          // 액세스 토큰 추출 및 저장
+          const accessToken = responseData.accessToken;
+
+          navigation.navigate('MainPageScreen');
+        }
+      } else {
+          if (response.status === 404) {
+            // 사용자를 찾을 수 없음
+            Alert.alert('로그인 실패', '사용자를 찾을 수 없습니다.');
+          }
+          else if (response.status === 400) {
+            // 잘못된 요청
+            Alert.alert('로그인 실패', '잘못된 요청입니다.');
+          } else if (response.status === 401) {
+            // 토큰이 유효하지 않음
+            Alert.alert('로그인 실패', '토큰이 유효하지 않습니다.');
+          } else if (response.status === 405) {
+            // 허용되지 않는 메소드로 요청됨
+            Alert.alert('로그인 실패', '허용되지 않는 메소드로 요청되었습니다.');
+          } else if (response.status === 409) {
+            // 비밀번호가 일치하지 않음
+            Alert.alert('로그인 실패', '비밀번호가 일치하지 않습니다.');
+          }
+      }
+    } catch (error) {
+      console.error('Error', error);
+      Alert.alert('오류', '서버와의 통신 중 오류가 발생했습니다.');
+    }
   };
 
   return (
@@ -29,11 +78,13 @@ const LoginScreen = () => {
           <InputTextField
             label="아이디"
             placeholder="5~15자 아이디를 입력하세요"
+            onChangeText={(text) => setAccountId(text)}
           />
           <InputTextField
             label="비밀번호"
             placeholder="8~20자 비밀번호를 입력하세요"
             secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
           />
         </View>
       <View style={styles.buttonContainer}>
