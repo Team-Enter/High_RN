be87035c67f30b8435fd0937ba926b99b2ad8de7
yang-yunho <yangyoonho0710@naver.com>
2024-02-09 01:06:10 +0900

    ⚡️ :: 사용자 정보 확인 API 연동

--- HIGH/src/screens/MyPageScreen.js
+++ HIGH/src/screens/MyPageScreen.js
@@ -1,18 +1,63 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, Modal, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal, Button, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import PeopleInfo from '../components/PeopleInfo';
 import MyPageButton from '../components/MyPageButton';
 import ModalLogOut from '../components/ModalLogOut';
 import ModalBug from '../components/ModalBug';
-import TopBarTitle from '../components/TopBarTitle';
+// import TopBarTitle from '../components/TopBarTitle';
 
 // 마이페이지
 const MyPageScreen = () => {
   const navigation = useNavigation();
   const [isLogoutModalVisible, setIsLogoutModalVisible] = useState(false);
   const [isBugReportModalVisible, setIsBugReportModalVisible] = useState(false);
+
+  const [userData, setUserData] = useState(null); // 사용자 정보 상태
+
+  // API 호출 및 데이터 가져오기
+  useEffect(() => {
+    fetchUserData(); // 페이지가 렌더링될 때 사용자 정보 가져오기
+  }, []);
+
+  // 사용자 정보 가져오는 함수
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/user/login');
+      if (response.ok) {
+        const data = await response.json();
+        setUserData(data);
+      } else if (response.status === 400){
+        // 잘못된 요청
+        const errorData = await response.json();
+        Alert.alert('잘못된 요청', errorData.message);
+      }
+      else if (response.status === 401){
+        // 토큰이 유효하지 않음
+        const errorData = await response.json();
+        Alert.alert('토큰 오류', errorData.message);
+      }
+      else if (response.status === 404){
+        // 요청한 페이지를 찾을 수 없음
+        const errorData = await response.json();
+        Alert.alert('페이지 찾기 오류', errorData.message);
+      }
+      else if (response.status === 405){
+        // 허용하지 않는 메소드로 요청함
+        const errorData = await response.json();
+        Alert.alert('허용되지 않는 메소드', errorData.message);
+      }
+      else if (response.status === 409){
+        // 비밀번호가 일치하지 않음
+        const errorData = await response.json();
+        Alert.alert('비밀번호 오류', errorData.message);
+      }
+    } catch (error) {
+      console.error('Error', error);
+      Alert.alert('오류', '서버와의 통신 중 오류가 발생했습니다.');
+    }
+  };
 
   const handleMainPagePress = () => {
     navigation.navigate('MainPageScreen');
